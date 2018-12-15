c8a47bebec8f6231a28dababd4bed75e39de135d
krisolew <krisolew@wp.pl>
2018-12-15 22:38:48 +0100

    Second version of Crawler added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        Spider spider = new Spider();
-        spider.search("http://arstechnica.com/", "computer");
+        //Spider spider = new Spider();
+        //spider.search("http://arstechnica.com/", "computer");
 
-
-        int a = 5;
+        WebCrawler crawler = new WebCrawler();
+        try {
+            crawler.serch("https://google.com");
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
